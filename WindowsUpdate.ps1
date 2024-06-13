66b6d065292842a545b43487bdfeee9dae03cf95
ticog <sebastianmihut@gmail.com>
2024-06-13 12:35:17 +0200

    blabla

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -2,16 +2,6 @@ Install-Module PSWindowsUpdate -Force -Confirm:$false
 Start-Sleep 5
 import-module PSWindowsUpdate
 
-$Update = (Get-WindowsUpdate -Title ".*23H2").KB
-
-if ($Update) {
-    write-host "[+] 23H2 Update wurde gefunden`n" -ForegroundColor Green
-    write-host "[+] Wird nun installiert...`n" -ForegroundColor Green
-    Install-WindowsUpdate -KBArticleID $Update -Hide -ForceInstall -IgnoreReboot
-} else {
-    continue
-}
-
 # Define the path to your script
 $scriptPath = "$env:HOMEPATH\Desktop\Client-Scripts\Test.ps1"
 # Create a new scheduled task action
@@ -22,5 +12,15 @@ $trigger = New-ScheduledTaskTrigger -AtLogOn
 $principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 # Create the scheduled task
 Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "LogonScript" -Description "Run script at logon"
+
+$Update = (Get-WindowsUpdate -Title ".*23H2").KB
+
+if ($Update) {
+    write-host "[+] 23H2 Update wurde gefunden`n" -ForegroundColor Green
+    write-host "[+] Wird nun installiert...`n" -ForegroundColor Green
+    Install-WindowsUpdate -KBArticleID $Update -Hide -ForceInstall -AutoReboot
+} else {
+    continue
+}
 
 Restart-Computer
