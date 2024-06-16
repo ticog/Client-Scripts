ca535d96ab75d828da54441c0a1d19bcab480b7e
unknown <sebastianmihut@gmail.com>
2024-06-16 18:00:37 +0200

    added version detection

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -1,24 +1,29 @@
-Write-Host "[!] PSWindowsUpdate Modul wird installiert..." -ForegroundColor Yellow
-Install-Module PSWindowsUpdate -Force -Confirm:$false
+$GetOSVersion = (Get-ComputerInfo).OSDisplayVersion
 
-try {
-    Import-module PSWindowsUpdate -ErrorAction Stop
-    Write-Host "[!] PSWindowsupdate Modul wurde installiert" -ForegroundColor Green
-} catch {
-    write-Host "[!] PSWindowsUpdate Modul konnte nicht installiert werden" -ForegroundColor Red
-}
+if ($GetOSVersion -ne "23H2") {
+    Write-Host "[!] PSWindowsUpdate Modul wird installiert..." -ForegroundColor Yellow
+    Install-Module PSWindowsUpdate -Force -Confirm:$false
+    try {
+        Import-module PSWindowsUpdate -ErrorAction Stop
+        Write-Host "[!] PSWindowsupdate Modul wurde installiert" -ForegroundColor Green
+    } catch {
+        write-Host "[!] PSWindowsUpdate Modul konnte nicht installiert werden" -ForegroundColor Red
+    }
+    write-host "[+] Updates werden nun installiert...`n" -ForegroundColor Green
+    Install-WindowsUpdate -AcceptAll -ForceInstall -AutoReboot
 
-$scriptPath = "C:\Script\WindowsReset.ps1"
-$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -NoLogo -WindowStyle Normal -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "AutoFactoryReset" -Description "At Startup The System will reset to Factory defaults"
-
-write-host "[+] Updates werden nun installiert...`n" -ForegroundColor Green
-Install-WindowsUpdate -AcceptAll -ForceInstall -AutoReboot
-
-if ( $env:USERNAME -eq "tico") {
-    exit 1
 } else {
-    Restart-Computer
+    
+    $scriptPath = "C:\Script\WindowsReset.ps1"
+    $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -NoLogo -WindowStyle Normal -File `"$scriptPath`""
+    $trigger = New-ScheduledTaskTrigger -AtStartup
+    $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+    Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "AutoFactoryReset" -Description "At Startup The System will reset to Factory defaults"
+    
+    
+    if ( $env:USERNAME -eq "tico") {
+        exit 1
+    } else {
+        Restart-Computer
+    }
 }
