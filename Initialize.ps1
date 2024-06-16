2d5f999923570f3977cbbc44c6b0b1a160177ebf
unknown <sebastianmihut@gmail.com>
2024-06-16 15:13:02 +0200

    iiiiii

--- Initialize.ps1
+++ Initialize.ps1
@@ -23,3 +23,4 @@ Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/add-package", "/pa
 
 Write-Host "[!] Es wird nun nach dem Neustart mit Windows Updates fortgefahren, bitte schalte das Geraet NICHT ab!" -ForegroundColor Green
 sleep 5
+Restart-Computer
