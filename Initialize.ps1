15b87e60e3475a1589b569352e31b1ed8b493c31
unknown <sebastianmihut@gmail.com>
2024-06-16 13:09:28 +0200

    aaaa

--- Initialize.ps1
+++ Initialize.ps1
@@ -1,7 +1,8 @@
 $path = "C:\Windows\Temp"
 
 Invoke-WebRequest -uri "https://github.com/ticog/Client-Scripts/archive/refs/heads/main.zip" -OutFile "$path\Client-Scripts.zip"
-Expand-Archive "$path\Client-Scripts.zip" "$path\Client-Scripts"    
+Expand-Archive "$path\Client-Scripts.zip" "$path"
+
 
 Write-Host "[!] Es wird nun mit Windows Updates fortgefahren, bitte schalte das Geraet NICHT ab!" -ForegroundColor Green
 powershell.exe -File "$path\Client-Scripts\WindowsUpdate.ps1"
