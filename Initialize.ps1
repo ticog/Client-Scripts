a47e82abe5db02785b2fbacba1b604c9a47e18ea
unknown <sebastianmihut@gmail.com>
2024-06-16 14:56:10 +0200

    Initialize has been changed

--- Initialize.ps1
+++ Initialize.ps1
@@ -1,10 +1,24 @@
+# Um den Script beim Client zu starten, benutze dieser Oneliner: 
+# (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ticog/Client-Scripts/main/Initialize.ps1" -UseBasicParsing).Content | powershell -c -
+
+New-Item -Path "C:\" -Name "Script" -ItemType Directory
+
 Install-PackageProvider -Name Nuget -Confirm:$false -Force -ForceBootstrap 
 $path = "C:\Windows\Temp"
 
 Invoke-WebRequest -uri "https://github.com/ticog/Client-Scripts/archive/refs/heads/main.zip" -OutFile "$path\Client-Scripts.zip"
 Expand-Archive "$path\Client-Scripts.zip" "$path"
 
+Copy-Item -Recurse "C:\Windows\Temp\Client-Scripts-main\*" "C:\Script\"
 
+Invoke-WebRequest -uri "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/49a41627-758a-42b7-8786-cc61cc19f1ee/public/windows11.0-kb5027397-x64_955d24b7a533f830940f5163371de45ff349f8d9.cab" -UseBasicParsing -OutFile "$path\23H2.cab"
+Move-Item "$path\windows11.0-kb5027397-x64_955d24b7a533f830940f5163371de45ff349f8d9.cab" "$path\23H2.cab"
+Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/add-package", "/packagepath:C:\Windows\Temp\23H2.cab" -NoNewWindow -Wait
+
+$scriptPath = "C:\Script\WindowsUpdate.ps1"
+$action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File `"$scriptPath`""
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "WindowsUpdate" -Description "At Startup The System will Update windows to 23H2"
 
 Write-Host "[!] Es wird nun mit Windows Updates fortgefahren, bitte schalte das Geraet NICHT ab!" -ForegroundColor Green
-powershell.exe -File "$path\Client-Scripts-main\WindowsUpdate.ps1"
--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -1,5 +1,3 @@
-New-Item -Path "C:\" -Name "Script" -ItemType Directory
-cp -Recurse "C:\Windows\Temp\Client-Scripts-main\*" "C:\Script\"
 Write-Host "[!] PSWindowsUpdate Modul wird installiert..." -ForegroundColor Yellow
 Install-Module PSWindowsUpdate -Force -Confirm:$false
 
@@ -9,17 +7,11 @@ try {
 } catch {
     write-Host "[!] PSWindowsUpdate Modul konnte nicht installiert werden" -ForegroundColor Red
 }
-Copy-Item "WindowsReset.ps1" "C:\Script\WindowsReset.ps1"
 
-# Define the path to your script
 $scriptPath = "C:\Script\WindowsReset.ps1"
-# Create a new scheduled task action
 $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File `"$scriptPath`""
-# Create a new scheduled task trigger for logon
 $trigger = New-ScheduledTaskTrigger -AtStartup
-# Define the principal (user) for whom the task will run
 $principal = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-# Create the scheduled task
 Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName "AutoFactoryReset" -Description "At Startup The System will reset to Factory defaults"
 
 write-host "[+] Updates werden nun installiert...`n" -ForegroundColor Green
