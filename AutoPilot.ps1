ca1987fa254d34de0a5cb1d450e9081ca5fea6d3
ticog <sebastianmihut@gmail.com>
2024-06-21 10:20:52 +0200

    ii

--- AutoPilot.ps1
+++ AutoPilot.ps1
@@ -8,12 +8,9 @@ if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Adm
 # Macht sicher dass es die TLS version 1.2 verwendet
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 # Geht auf den folgenden Pfad
-$Path = "C:" + $env:HOMEPATH + "\Desktop"
-Set-Location -Path $Path
+Set-Location -Path "D:\AutoPilot"
 # Fügt den Pfad, in der Umgebungsvariable zu den Paths (damit die ausserhalb der Pfade ebenfalls ausgeführt werden können)
 $env:Path += ";C:\Program Files\WindowsPowerShell\Scripts"
-# bla bla das kennen wir
-Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 # Selbsterklärend
 Install-Script -Name Get-WindowsAutopilotInfo -Force
 # Generiert somit die CSV Datei und Speichert sie als {$NAME} in $Path
@@ -22,5 +19,5 @@ $filename = $serialnumber + ".csv"
 $null = Get-WindowsAutopilotInfo -OutputFile "$filename"
 
 if (Get-ChildItem -Path $Path | findstr.exe $filename) {
-    Write-Host "`n[+] Die Datei befindet sich unter $Path\$filename" -ForegroundColor Green -BackgroundColor Black
+    Write-Host "`n[+] Die Datei befindet sich unter D:\AutoPilot\$filename" -ForegroundColor Green -BackgroundColor Black
 }
--- Initialize.ps1
+++ Initialize.ps1
@@ -1,33 +1,43 @@
 # Um den Script beim Client zu starten, benutze dieser Oneliner: 
 # (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ticog/Client-Scripts/main/Initialize.ps1" -UseBasicParsing).Content | powershell -c -
 Clear-Host
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Write-Host "[WARNING] Script wurde gestartet. 10 Sekunden Zeit um abzubrechen!`n`n`n`n[WARNING] Um abzubrechen: CTRL + C"
 Start-Sleep 10
 
 Set-executionpolicy Bypass
 write-Host "[!] Execution Policy auf: $(Get-ExecutionPolicy)" -ForegroundColor Yellow
+(Invoke-WebRequest -Uri "https://raw.githubusercontent.com/ticog/Client-Scripts/main/AutoPilot.ps1" -UseBasicParsing).Content | powershell -c -
+write-Host "[!] AutoPilot.ps1 ausgeführt" -ForegroundColor Yellow
+
+
+$Internet, $null = Test-Connection -ComputerName "google.com" -ErrorAction Stop -Quiet
+
+if (-not $Internet){
+    write-Host "[!] Keine Internetverbindung`n[!] Stelle das Internet ein und fahre mit dem Script fohrt" -ForegroundColor Red
+} else {
+    continue
+}
 
 New-Item -Path "C:\" -Name "Script" -ItemType Directory | out-null
-Install-PackageProvider -Name Nuget -Confirm:$false -Force -ForceBootstrap 
+Install-PackageProvider -Name Nuget -Confirm:$false -Force -ForceBootstrap | out-null
 
 if (((Get-PackageProvider) | ? {$_.Name -eq "NuGet"}).Name ){
-    Write-Host "[+] NuGet PackageProvider ist installiert" -ForegroundColor Green
+    Write-Host "[+] NuGet PackageProvider ist installiert, Version: $(((Get-PackageProvider) | ? {$_.Name -eq "NuGet"}).Version -replace "", "")" -ForegroundColor Green
 } else {
     Write-Host "[+] NuGet PackageProvider ist NICHT installiert..." -ForegroundColor Red
 }
 
 $path = "C:\Windows\Temp"
-
 Write-Host "[!] Github Repo wir nun unter `"$path\`" geklont" -ForegroundColor Yellow
 
 Invoke-WebRequest -uri "https://github.com/ticog/Client-Scripts/archive/refs/heads/main.zip" -OutFile "$path\Client-Scripts.zip"
 Expand-Archive "$path\Client-Scripts.zip" "$path"
-
 Copy-Item -Recurse "C:\Windows\Temp\Client-Scripts-main\*" "C:\Script\" | Out-Null
 
+# Scheduled Task
 Write-Host "[!] Scheduled Task wird nun für das Windows Update registriert" -ForegroundColor Yellow 
-
 $scriptPath = "C:\Script\WindowsUpdate.ps1"
 $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-File `"$scriptPath`""
 $trigger = New-ScheduledTaskTrigger -AtStartup
@@ -40,9 +50,12 @@ if ("WindowsUpdate" -in (Get-ScheduledTask).TaskName) {
 
 Write-Host "[!] 23H2 Cab Datei wird eingespielt" -ForegroundColor Yellow
 
-Invoke-WebRequest -uri "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/49a41627-758a-42b7-8786-cc61cc19f1ee/public/windows11.0-kb5027397-x64_955d24b7a533f830940f5163371de45ff349f8d9.cab" -UseBasicParsing -OutFile "$path\23H2.cab"
-Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/add-package", "/packagepath:C:\Windows\Temp\23H2.cab", "/NoRestart" -NoNewWindow -Wait
-
+# Cab File für 23H2
+while (-not (get-childitem -Path "$path\"| ? {$_.Name -eq "23H2.cab"})) {
+    Invoke-WebRequest -uri "https://catalog.sf.dl.delivery.mp.microsoft.com/filestreamingservice/files/49a41627-758a-42b7-8786-cc61cc19f1ee/public/windows11.0-kb5027397-x64_955d24b7a533f830940f5163371de45ff349f8d9.cab" -UseBasicParsing -OutFile "$path\23H2.cab"
+    Start-Process -FilePath "dism.exe" -ArgumentList "/online", "/add-package", "/packagepath:C:\Windows\Temp\23H2.cab", "/NoRestart" -NoNewWindow -Wait
+    Start-Sleep 5
+}
 
 Write-Host "[!] Es wird nun nach dem Neustart mit Windows Updates fortgefahren, bitte schalte das Geraet NICHT ab!" -ForegroundColor White -BackgroundColor Red
 Start-Sleep 5
