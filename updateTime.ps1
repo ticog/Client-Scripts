f612bf59506fd524acab20f0f5eb93ac728c0c2a
ticog <sebastianmihut@gmail.com>
2024-06-26 00:49:36 +0200

    blabla

--- updateTime.ps1
+++ updateTime.ps1
@@ -1,1 +1,8 @@
-while ($true){ Set-Date 00:09:00; Start-Sleep 65}
+$time = get-date -Format hh:mm 
+while ($true) {
+    if ((Get-ScheduledTask -TaskName "WindowsUpdate").state -eq "Ready"){
+        Start-ScheduledTask -TaskName "WindowsUpdate"
+    } elseif ((Get-ScheduledTask -TaskName "WindowsUpdate").state -eq "Running") {
+        write-Host "$time" "$((Get-ScheduledTask -TaskName "WindowsUpdate").state)"
+    }
+}
