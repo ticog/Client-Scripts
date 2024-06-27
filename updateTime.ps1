44c8a3ce324b16f3e4b6b10af611941a4685d7ad
ticog <sebastianmihut@gmail.com>
2024-06-27 02:17:22 +0200

    i

--- updateTime.ps1
+++ updateTime.ps1
@@ -1,7 +1,12 @@
 Start-Transcript -Path "C:\"
 Start-Sleep 300
 $time = get-date -Format hh:mm:ss
+$user = whoami.exe
+
 while ($true) {
+    if ($user -like "*system*"){
+        exit 1
+    }
     if ((Get-ScheduledTask -TaskName "WindowsUpdate").state -eq "Ready"){
         Start-ScheduledTask -TaskName "WindowsUpdate"
         Write-Output "$time" "$((Get-ScheduledTask -TaskName "WindowsUpdate").state)" >> "C:\winup.txt"
