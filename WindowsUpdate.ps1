7f2a2068c15f4263f3afba8ac9b2f9ec211972da
Sebastian Masis <sebastianmihut@gmail.ch>
2024-06-28 14:01:09 +0200

    iiii

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -12,18 +12,24 @@ if ($GetOSVersion -ne "23H2") {
     }
     Start-Sleep 5
     Get-WUList > C:\Updates.txt
-    $kb = ((Get-Content C:\Updates.txt | Select-String ".*23H2").Matches.Value | Select-String "KB[0-9]+").Matches.Value
+    if ((Get-Content C:\Updates.txt | Select-String "23H2").Matches.Value -eq "23H2"){ 
+        Write-Host "[!] 23H2 gefunden!"
+        $kb = ((Get-Content C:\Updates.txt | Select-String ".*23H2").Matches.Value | Select-String "KB[0-9]+").Matches.Value
+    } else { 
+        Start-Sleep 600
+    }
+    
     write-host "[+] Updates werden nun installiert...`n" -ForegroundColor Green
     Install-WindowsUpdate -KBArticleID $kb -ForceInstall -Confirm:$false -AutoReboot
+
     if ((Get-WUHistory | Select-Object title | Select-String -AllMatches "Windows 11, version 23H2").Matches.Value){
         Restart-Computer
+    } else {
+        start-process powershell.exe -Argument "-Command `"[console]::beep(1000, 100)`""
     }
-    Restart-Computer
 
 } elseif ($GetOSVersion -eq "23H2") {
     Invoke-WebRequest -UseBasicParsing -Uri $PSexecLink -OutFile "C:\Windows\Temp\psexec.zip"
     Expand-Archive "C:\Windows\Temp\psexec.zip" "C:\Script\"
     Start-Process -FilePath "C:\Script\psexec.exe" -ArgumentList "-accepteula", "-S powershell.exe", "-File `"C:\Script\WindowsReset.ps1`""
 }
-
-Restart-Computer
